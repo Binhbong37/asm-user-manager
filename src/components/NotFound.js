3eeae08884cff00554925e46b26b35cc806762e8
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2023-02-08 09:49:37 +0700

    12. modify page not found

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,23 +3,17 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div
-            class="card shadow p-3 mb-5 bg-white rounded"
-            style={{ width: '18rem;' }}
+            className="container card shadow p-3 mt-5 bg-white rounded text-center"
+            style={{ width: '30rem' }}
         >
-            <div class="card-body">
-                <h5 class="card-title">404</h5>
-                <h6 class="card-subtitle mb-2 text-muted">
+            <div className="card-body">
+                <h5 className="card-title">404</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
                     OOPS! PAGE NOT FOUD
                 </h6>
-                <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                </p>
-                <a href="/#" class="card-link">
-                    Card link
-                </a>
-                <a href="/#" class="card-link">
-                    Another link
+                <p className="card-text">Back to home and try again!!!</p>
+                <a href="/" className="card-link">
+                    Back to home
                 </a>
             </div>
         </div>
